2f00f212f91e5a65a79a92074e9f421194a5f341
e-oheredia <oheredia@exact.com.pe>
2020-02-13 13:01:35 -0500

    Implementación de Campañas en DEMO

--- service-tipos-documentos/src/main/java/com/exact/service/documentos/controller/TipoDocumentoController.java
+++ service-tipos-documentos/src/main/java/com/exact/service/documentos/controller/TipoDocumentoController.java
@@ -3,8 +3,6 @@ package com.exact.service.documentos.controller;
 import java.io.IOException;
 import java.util.List;
 
-
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
